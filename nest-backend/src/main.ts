cb61ba734a971cc7cfcc6b7d996b27462dc2c7c9
SherwinBenjamin <sherwinbenjamin2828@gmail.com>
2025-01-07 02:21:45 +0530

    cors fix

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); 
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: "https://multi-tenant-rbac.vercel.app",
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());  
